b10d4208ea1d7a485935af6212c8bee3cc403bca
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-22 12:10:22 +0600

    string e index diya set kora jai na

--- string.js
+++ string.js
@@ -1,4 +1,15 @@
-const cup ='world cup';
-const tea = 'tea cup';
-const location = 'bandor bon';
-console.log(typeof cup)
+const cup ='World cup';
+const tea = "tea cup";
+const location = `Dhaka`;
+
+
+
+// const numbers = [4,3,5,6,7,8,9,1]
+// // console.log(numbers.length)
+// // console.log(cup.length)
+// console.log(numbers[4])
+// console.log(cup[4])
+// console.log(tea.length);
+// console.log(tea[4]);
+location[0]= 'F';
+console.log(location);
