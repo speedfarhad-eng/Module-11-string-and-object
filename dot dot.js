f16bc077fbd9b23cb61ace4c13168c283802848e
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-22 19:43:26 +0600

    dot concept

---
+++ dot dot.js
@@ -0,0 +1,11 @@
+const person={
+    name: 'jamil',
+    age : 25,
+    profession : 'student',
+    isSingle : true
+}
+const age =person.age;
+console.log(person);
+person.age = 29;
+person.isSingle =false;
+console.log(person)
---
+++ object.js
@@ -0,0 +1,7 @@
+const student ={
+    Name : 'joynal abein farhad',
+    Batch : "71D",
+    Roll : 4958,
+    active : true 
+}
+console.log(student)
