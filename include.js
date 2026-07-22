d795cc1b5d1ee326a870022a422f25c1aaf1774a
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-22 15:57:23 +0600

    includes concept

---
+++ include.js
@@ -0,0 +1,17 @@
+// const email= "mama@mami.com"
+// if(email.includes('.com')){
+//     console.log('ache na cha bheda')
+// }
+
+// const fileName = 'amrfile.png'
+// console.log(fileName.endsWith('.png'))
+
+
+// const email= 'siam@gamil.com'
+// if(email.includes('gamil')){
+//     console.log("hawarpot dekha taratari")
+// }
+const email = 'farhad@gamil.com'
+if(email.includes('farhad')){
+    console.log("taile bolo aro jore")
+}
