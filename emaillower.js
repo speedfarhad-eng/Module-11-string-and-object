f651c91c8ae2c36f636f875d4c69d372fd405eb5
farhad6366 <0325714958@student.wub.edu.bd>
2026-07-22 15:37:29 +0600

    uppercase and trim section

---
+++ emaillower.js
@@ -0,0 +1,8 @@
+const emailUserprovided = "    goni@mia.com   "
+const emailSavedIndatabase = 'goni@mia.com'
+console.log(emailUserprovided.trim().toUpperCase())
+if (emailUserprovided.toUpperCase === emailSavedIndatabase.toUpperCase){
+    console.log("vaiya ami uppar")
+}else{
+    console.log("ami bujtesi na vaiya")
+}
